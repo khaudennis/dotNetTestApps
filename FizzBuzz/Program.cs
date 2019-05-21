1166d34ed2f15b05dc9d347e83365fe22f0ba1aa
Dennis Khau <iam@denniskhau.com>
2019-05-20 20:19:24 -0400

    Initial commit of .NET test apps

---
+++ FizzBuzz/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Text;
+using System.Threading;
+
+namespace FizzBuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 1;
+
+            for (i = 1; i <= 100; i++)
+            {
+                if ((i % 3 == 0) && (i % 5 == 0)) { Console.WriteLine("FizzBuzz\n"); }
+                else if (i % 3 == 0) { Console.WriteLine("Fizz\n"); }
+                else if (i % 5 == 0) { Console.WriteLine("Buzz\n"); }
+                else { Console.WriteLine(i.ToString() + "\n"); }
+
+                Thread.Sleep(1000);
+            }
+
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ResponseStream/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Text;
+using System.Threading;
+
+namespace ResponseStream
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Provide resource and  hit [Enter]:\n"); //line terminator character
+
+                WebRequest req = WebRequest.Create(Console.ReadLine());
+                WebResponse resp = req.GetResponse();
+                Stream stream = resp.GetResponseStream();
+                Encoding enc = System.Text.Encoding.GetEncoding("utf-8");
+                StreamReader sr = new StreamReader(stream, enc);
+
+                Console.WriteLine("\nResponse Stream Received...\n");
+                Thread.Sleep(5000);
+
+                Char[] read = new Char[256];
+                int count = sr.Read(read, 0, 256);
+
+                while (count > 0)
+                {
+                    string str = new string(read, 0, count);
+                    Console.Write(str);
+                    Thread.Sleep(1000);
+                    count = sr.Read(read, 0, 256);
+                }
+
+                sr.Close();
+                resp.Close();
+
+                Console.WriteLine("Press any key to exit.\n");
+                Console.ReadKey();
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("\nException encountered...\n");
+                Console.WriteLine("Press any key to exit.\n");
+                Console.ReadKey();
+            }
+
+        }
+    }
+}
